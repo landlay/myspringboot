c985995fa9a4c52c6efdf10209d273eef115f936
1053385770@qq.com <1053385770@qq.com>
2020-02-16 17:52:16 +0800

    add https

--- src/main/java/com/landkay/springboot/schedule/BaiDuScheduled.java
+++ src/main/java/com/landkay/springboot/schedule/BaiDuScheduled.java
@@ -1,7 +1,10 @@
 package com.landkay.springboot.schedule;
 
 import com.landkay.springboot.utils.HttpUtils;
+import com.landkay.springboot.utils.SSLClient;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -18,6 +21,8 @@ import java.util.Map;
 @EnableScheduling
 public class BaiDuScheduled {
 
+    private final static Logger log = LoggerFactory.getLogger(BaiDuScheduled.class);
+
     /**
      * Description // 秒、分、时、日、月、年
      * @param
@@ -28,12 +33,17 @@ public class BaiDuScheduled {
     @Scheduled(cron = "0/10 * * * * ?")
     public void pushDataScheduled() {
 
-        String url = "http://baijiahao.baidu.com/s?id=1607639342822187726";
+        String url = "https://mbd.baidu.com/newspage/data/landingsuper?context=%7B%22nid%22%3A%22news_9590297299213189222%22%7D&n_type=1&p_from=3";
         Map<String, Object> paramsMap = new HashMap(8);
         try {
-            StringBuffer stringBuffer = HttpUtils.URLGet(url, paramsMap);
-            log.info("访问百家号响应参数: " + stringBuffer.toString());
-        } catch (IOException e) {
+            //往服务端写数据
+            String outStr = null;
+            String get = SSLClient.httpsRequest(url, "GET", outStr);
+
+            //StringBuffer stringBuffer = HttpUtils.URLGet(url, paramsMap);
+            //log.info("访问百家号响应参数: " + stringBuffer.toString());
+            log.info("访问百家号响应参数: " + get);
+        } catch (Exception e) {
             log.error(e.getMessage());
         }
     }
